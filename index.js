1905a955cd1b3bb9879e064d4b3846971995fdea
Elly <ellyjames1999@gmail.com>
2024-12-02 13:11:47 +0300

    finished them

--- index.js
+++ index.js
@@ -1,1 +1,97 @@
 // Code your solution in this file!
+/*
+function distanceFromHqInBlocks(pickUpLocation) {
+    const hqBlocks= 42;
+    let numberOfBlocksFromPickUp= Math.abs(hqBlocks-pickUpLocation); //returns an absolute value of the blocks
+    return numberOfBlocksFromPickUp;
+    
+  }
+  
+
+  
+function distanceFromHqInFeet(pickUpLocation){
+    const block = distanceFromHqInBlocks(pickUpLocation); //we call the function here to be able to use it in this function
+    const feetPerBlock= 264;
+    
+    return block*feetPerBlock;
+
+}
+
+function distanceTravelledInFeet(start, destination){
+   
+    return (Math.abs(destination-start))*264;
+    
+
+
+}
+
+function calculatesFarePrice(start, destination){
+    const feetTravelled = distanceTravelledInFeet(start, destination);
+    if(feetTravelled<=400){
+        return 0
+    }
+    else if (feetTravelled>400 && feetTravelled<= 2000){
+        const distanceToCharge= feetTravelled -400;
+
+        return distanceToCharge*0.02;
+    } 
+    else if(feetTravelled>2000 && feetTravelled<=2500){
+        return 25;
+    }
+    else {
+        return 'cannot travel that far'
+    }
+
+
+
+}
+ 
+
+
+
+  console.log("Distance in blocks is: " + distanceFromHqInBlocks(34) +"blocks");
+
+  console.log("Distance in feet is: " + distanceFromHqInFeet(34) + "feet");
+
+  console.log("Distance Travelled in feet is: " + distanceTravelledInFeet(34, 42) + "feet");
+
+  console.log("Fare price is:" + calculatesFarePrice(34, 42));
+  */
+
+  function distanceFromHqInBlocks(pickUpLocation) {
+    const hqBlocks = 42;
+    return Math.abs(hqBlocks - pickUpLocation); // Returns absolute value of the blocks
+}
+
+function distanceFromHqInFeet(pickUpLocation) {
+    const feetPerBlock = 264;
+    return distanceFromHqInBlocks(pickUpLocation) * feetPerBlock;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    const feetPerBlock = 264;
+    return Math.abs(destination - start) * feetPerBlock;
+}
+
+function calculatesFarePrice(start, destination) {
+    const feetTravelled = distanceTravelledInFeet(start, destination);
+
+    if (feetTravelled <= 400) {
+        return 0; // Free ride for distances <= 400 feet
+    } else if (feetTravelled > 400 && feetTravelled <= 2000) {
+        const chargeableDistance = feetTravelled - 400;
+        return chargeableDistance * 0.02; // $0.02 per foot
+    } else if (feetTravelled > 2000 && feetTravelled <= 2500) {
+        return 25; // Flat rate for distances between 2001 and 2500 feet
+    } else {
+        return 'cannot travel that far'; // Exceeds 2500 feet
+    }
+}
+
+// Testing the functions
+console.log("Distance in blocks is: " + distanceFromHqInBlocks(34) + " blocks");
+console.log("Distance in feet is: " + distanceFromHqInFeet(34) + " feet");
+console.log("Distance Travelled in feet is: " + distanceTravelledInFeet(34, 42) + " feet");
+console.log("Fare price is: " + calculatesFarePrice(34, 42));
+
+
